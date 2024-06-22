f1ae35589a9241059be0325e53d4a0727a744223
sripadsirik <sripadsirik@gmail.com>
2024-06-22 11:12:14 -0500

    splitting traffic data

--- modeltraining.py
+++ modeltraining.py
@@ -7,3 +7,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 #load the processed data
 df_traffic = pd.read_csv('processed_traffic_data.csv')
 
+
+# Split the data into features (X) and target (y)
+X = df_traffic.drop(columns=['timestamp', 'traffic_flow'])
+y = df_traffic['traffic_flow']
