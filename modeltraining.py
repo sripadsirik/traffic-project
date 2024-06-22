8aed4d326fd444341aca5c672a734019937c6888
sripadsirik <sripadsirik@gmail.com>
2024-06-22 11:13:00 -0500

    feat: Train Random Forest Regressor model and make predictions on test set

--- modeltraining.py
+++ modeltraining.py
@@ -14,3 +14,10 @@ y = df_traffic['traffic_flow']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a Random Forest Regressor model
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
