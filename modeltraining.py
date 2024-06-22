f92a61ba77a5a935b07d1ee35450645ccbea68a0
sripadsirik <sripadsirik@gmail.com>
2024-06-22 11:12:36 -0500

    feat: Split data into training and testing sets

--- modeltraining.py
+++ modeltraining.py
@@ -11,3 +11,6 @@ df_traffic = pd.read_csv('processed_traffic_data.csv')
 # Split the data into features (X) and target (y)
 X = df_traffic.drop(columns=['timestamp', 'traffic_flow'])
 y = df_traffic['traffic_flow']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
