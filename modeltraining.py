b19e5d27c298457f6f46564f991fc21f1f5fbc2f
sripadsirik <sripadsirik@gmail.com>
2024-06-22 11:10:57 -0500

    loading data

--- modeltraining.py
+++ modeltraining.py
@@ -3,3 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+
+#load the processed data
+df_traffic = pd.read_csv('processed_traffic_data.csv')
+
